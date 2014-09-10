76a8e087ab649cf38fc6c4f7ff68badeffc809c5
Sarveb Tools <sarveb.tools@gmail.com>
2014-09-10 16:32:41 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintrex::Application.config.secret_key_base = '8fd38a3b0665587e0c2f2d0587e043a9a3caff3708e426da24a2e6861d824aae2de2d26c44cec04e3f739425e8bef837a952df655ce8d4a71f1a9556a386e985'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pintrex::Application.config.session_store :cookie_store, key: '_pintrex_session'
